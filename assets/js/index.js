44f8fb42ae9bc06771a9c3409ec0a9c70c5e3b12
G4rutti <davigarutti5@gmail.com>
2023-01-09 19:15:14 -0300

    Linkando a API com o site - site está sem nada yet

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+const fetchPl =  async() => {
+    const APIResponse = await fetch(`http://localhost:3000/pl`)
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
+}
+
+const renderTopLeaders = async() => {
+    const data = await fetchPl()
+    console.log(data)
+}
+
+renderTopLeaders()
