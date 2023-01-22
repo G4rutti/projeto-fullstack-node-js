a3cfa71e4d0939330783e8c3f8127f7c50b42ccc
G4rutti <davigarutti5@gmail.com>
2023-01-22 19:52:24 -0300

    lógica concluida

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
-const fetchPl =  async() => {
-    const APIResponse = await fetch(`http://localhost:3000/pl`)
+const fetchApi=  async(liga) => {
+    const APIResponse = await fetch(`http://localhost:3000/${liga}`)
     if (APIResponse.status === 200) {
         const data = await APIResponse.json();
         return data;
@@ -7,8 +7,18 @@ const fetchPl =  async() => {
 }
 
 const renderTopLeaders = async() => {
-    const data = await fetchPl()
-    console.log(data['leaders'])
+    const data = await fetchApi(document.getElementById('liga').value)
+    document.getElementById('table').innerHTML = ""
+    for(let i = 0; i < parseInt(document.getElementById("mostrarQuantos").value); i++){
+        document.getElementById('table').innerHTML += `
+        <th>${data['leaders'][i]['posicaoRanking']}</th>
+        <td>${data['leaders'][i]['nomeArtilheiro']}</td>
+        <td>${data['leaders'][i]['nacionalidade']}</td>
+        <td>${data['leaders'][i]['idade']}</td>
+        <td>${data['leaders'][i]['clube']}</td>
+        <td>${data['leaders'][i]['jogos']}</td>
+        <td>${data['leaders'][i]['gols']}</td>
+        `
+    }
 }
 
-renderTopLeaders()
